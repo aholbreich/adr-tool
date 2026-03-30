6f9e5db232a93b79be7f8d832e577ed5f49e06c4
Alexander Holbreich <alexander@holbreich.org>
2026-03-30 18:50:06 +0200

    Fix CLI command error propagation

--- cli/Init_cmd.go
+++ cli/Init_cmd.go
@@ -12,21 +12,18 @@ type InitCmd struct {
 
 // Command Handler
 func (r *InitCmd) Run() error {
-
 	pathResolver := config.PathResolverInst()
 
 	if !pathResolver.IsFilepathGitRepo() {
 		if !confirmAction(".git folder is not detected. This does not seem to be the root of your project. Do you still want to proceed (Y/n)?") {
-			fmt.Printf("Initialization aborted by the user.\n")
+			fmt.Println("Initialization aborted by the user.")
 			return nil
 		}
 	}
 
 	mgr := config.NewConfigManager()
-
 	if err := mgr.InitConfig(); err != nil {
-		fmt.Println("Failed to initialize ADRs:", err)
-		return nil
+		return fmt.Errorf("initialize ADRs: %w", err)
 	}
 
 	fmt.Println("ADR initialized successfully at", pathResolver.ConfigFolderPath())
--- cli/new_cmd.go
+++ cli/new_cmd.go
@@ -17,25 +17,19 @@ type NewCmd struct {
 func (r *NewCmd) Run() error {
 	adrName := strings.Join(r.AdrName, " ")
 
-	// Load configuration using ConfigManager
 	cfgManager := config.NewConfigManager()
 	currentConfig, err := cfgManager.LoadConfig()
 	if err != nil {
-		fmt.Println("No ADR configuration found!")
-		fmt.Println("Start by initializing ADR configuration, check 'adr init --help' for more help")
-		return err
+		return fmt.Errorf("load ADR configuration: %w; run 'adr init' first", err)
 	}
 
-	// Increment ADR number and update config
 	currentConfig.CurrentAdr++
 	if err := cfgManager.UpdateConfig(currentConfig); err != nil {
-		return fmt.Errorf("failed to update config: %w", err)
+		return fmt.Errorf("update ADR config: %w", err)
 	}
 
-	// Create the new ADR
-
 	if err := adr.NewAdrManager().CreateNewAdr(currentConfig, adrName); err != nil {
-		return fmt.Errorf("failed to create new ADR: %w", err)
+		return fmt.Errorf("create new ADR: %w", err)
 	}
 
 	fmt.Printf("New ADR %03d was successfully written to: %s\n", currentConfig.CurrentAdr, currentConfig.BaseDir)
